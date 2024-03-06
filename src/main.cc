860f39c11ab8a836611461afd6a7e1fc9503a25a
Erick Pfeifer <erick.pfeifer@gmail.com>
2024-03-05 23:58:14 -0600

    Refactor main function to remove initial manual tests

--- src/main.cc
+++ src/main.cc
@@ -5,32 +5,13 @@
 
 int main() {
   SYS_Initialize(nullptr);
+  INF("Startup instantiations started.");
   tupa::sqr_wave::SqrWave wave(tupa::gpio::GetTCC0PinState);
 
-  INF("Main loop...");
-  MOTOR_ENABLE_Set();  // Motor is disabled.
+  INF("Startup instantiations done.");
 
   while (true) {
     LED_Toggle();
-
-    // Left button is pressed, right is not.
-    if(!BTN_LEFT_Get() && BTN_RIGHT_Get())  {
-      if(LOW_LIMIT_SENSOR_Get()) {
-        MOTOR_ENABLE_Clear();  // Motor is enabled.
-        MOTOR_DIR_Set();  // Goes Down.
-        wave.SetEnable(true);
-      } else {
-        wave.SetEnable(false);
-        MOTOR_ENABLE_Set();  // Motor is disabled.
-      }
-    } else if (BTN_LEFT_Get() && !BTN_RIGHT_Get()) {
-      MOTOR_ENABLE_Clear();  // Motor is enabled.
-      MOTOR_DIR_Clear();  // Goes Up.
-      wave.SetEnable(true);
-    } else {
-      wave.SetEnable(false);
-      MOTOR_ENABLE_Set();  // Motor is disabled.
-    }
   }
   return 0;
 }
