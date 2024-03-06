4ec9c7d6f5ee07be5699e9aa962f687a96bf04aa
Erick Pfeifer <erick.pfeifer@gmail.com>
2024-03-05 22:42:35 -0600

    Add wrapper funcitons for GPIOs

--- src/bsp/wrapper/gpio/gpio.cc
+++ src/bsp/wrapper/gpio/gpio.cc
@@ -1,5 +1,6 @@
 #include "gpio/gpio_wrapper.h"
 #include "same54n20a.h"
+#include "peripheral/port/plib_port.h"
 
 namespace tupa::gpio {
 
@@ -7,5 +8,30 @@ bool GetTCC0PinState() {
   PORT_REGS->GROUP[2].PORT_PINCFG[4] |= 0x02;
   return (((PORT_REGS->GROUP[2].PORT_IN >> 4U)) & 0x01U);
 }
+
+bool GetBtnLeftPinState() {
+  return BTN_LEFT_Get() != 0;
+}
+
+bool GetBtnRightPinState() {
+  return BTN_RIGHT_Get() != 0;
+}
+
+bool GetLowLimitSensorPinState() {
+  return LOW_LIMIT_SENSOR_Get() != 0;
+}
+
+void SetDbgPin(bool state) {
+  state ? DBG_Set() : DBG_Clear();
+}
+
+void SetMotorEnablePin(bool state) {
+  state ? MOTOR_ENABLE_Set() : MOTOR_ENABLE_Clear();
+}
+
+void SetBoardLEDPin(bool state) {
+  state ? LED_Set() : LED_Clear();
+}
+
 
 }  // namespace tupa::gpio
--- src/bsp/wrapper/gpio/public/gpio/gpio_wrapper.h
+++ src/bsp/wrapper/gpio/public/gpio/gpio_wrapper.h
@@ -2,6 +2,57 @@
 
 namespace tupa::gpio {
 
+/**
+ * @brief Gets pin state of the TCC0 wave generator.
+ *
+ * @return true
+ * @return false
+ */
 bool GetTCC0PinState();
+
+/**
+ * @brief Get the Btn Left Pin State.
+ *
+ * @return true
+ * @return false
+ */
+bool GetBtnLeftPinState();
+
+/**
+ * @brief Get the Btn Right Pin State.
+ *
+ * @return true
+ * @return false
+ */
+bool GetBtnRightPinState();
+
+/**
+ * @brief Get the Low Limit Sensor Pin State.
+ *
+ * @return true
+ * @return false
+ */
+bool GetLowLimitSensorPinState();
+
+/**
+ * @brief Set the debug pin state
+ *
+ * @param state True/False for high/low states.
+ */
+void SetDbgPin(bool state);
+
+/**
+ * @brief Set the Motor Enable Pin.
+ *
+ * @param state True/False for high/low states.
+ */
+void SetMotorEnablePin(bool state);
+
+/**
+ * @brief Set the Board LED Pin.
+ *
+ * @param state True/False for high/low states.
+ */
+void SetBoardLedPin(bool state);
 
 }  // namespace tupa::gpio
