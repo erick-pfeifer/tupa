376592f537ed93193e0d3c28a77e53813692176a
Erick Pfeifer <erick.pfeifer@gmail.com>
2024-03-07 21:36:21 -0600

    Add boilerplate for buttons logic, and created initial unit tests (not passing yet)

--- src/app/buttons/buttons.cc
+++ src/app/buttons/buttons.cc
@@ -1,2 +1,9 @@
 
-namespace tupa::buttons {}  // namespace tupa::buttons
+#include "buttons/buttons.h"
+
+#include "pw_status/status.h"
+namespace tupa::buttons {
+
+pw::Status Buttons::Process() { return pw::OkStatus(); }
+
+}  // namespace tupa::buttons
